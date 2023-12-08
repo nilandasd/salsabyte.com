c96080dc89cee3559866b1f73ca989bca07f8781
Niland Schumacher <naschuma@ucsc.edu>
2023-12-08 15:26:14 -0800

    shouldn't have been a .. :(

--- tools/upload_public.sh
+++ tools/upload_public.sh
@@ -5,4 +5,4 @@ npm run prod:webpack
 aws s3api put-object                      \
   --bucket salsabyte-public-folder-bucket \
   --key main.js                           \
-  --body ../public/dist/main.js
+  --body ./public/dist/main.js
