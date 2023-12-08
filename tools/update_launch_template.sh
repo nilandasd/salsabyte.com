f615f52d45ec6a3a22a7792fbfd46c981bcf0e16
Niland Schumacher <naschuma@ucsc.edu>
2023-12-07 21:59:41 -0800

    update security group :)

--- tools/update_launch_template.sh
+++ tools/update_launch_template.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+SG_ID=$(aws ec2 describe-security-groups \
+  --filters Name=group-name,Values=ec2 \
+  --query 'SecurityGroups[0].GroupId' \
+  --output text)
+
 BACKEND_AMI=`aws ec2 describe-images   \
   --filters "Name=name,Values=backend" \
   --query 'Images[0].[ImageId]' \
@@ -9,5 +14,10 @@ aws ec2 create-launch-template-version \
   --launch-template-name backend \
   --launch-template-data "{
       \"ImageId\": \"$BACKEND_AMI\",
-      \"InstanceType\": \"t3.micro\"
-    }"
+      \"InstanceType\": \"t3.micro\",
+      \"NetworkInterfaces\": [
+        {
+          \"Groups\": [\"$SG_ID\"]
+        }
+      ]
+    }" \
